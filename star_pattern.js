863f27503a1106b3421867ea19252daee575fbe1
KRLEESEUNGJAE <karmaup0711@gmail.com>
2021-06-26 20:02:32 +0900

    add 2개씩 추가

--- star_pattern.js
+++ star_pattern.js
@@ -1,3 +1,4 @@
+// 기본 별찍기
 var star = '';
 for (var i = 0; i < 10; i++) {
     for (var j = 0; j <= i; j++) {
@@ -5,4 +6,16 @@ for (var i = 0; i < 10; i++) {
     }
     star += '\n';
 }
+console.log('<기본>');
+console.log(star);
+
+// 2개씩 추가되는 별찍기
+var star = '';
+for (var i = 0; i < 10; i++) {
+    for (var j = 0; j <= (i * 2); j++) {
+        star += '*';
+    }
+    star += '\n';
+}
+console.log('<2개씩추가>');
 console.log(star);
