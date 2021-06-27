5facfb433a53d4dd6553725ade10985a20c86031
KRLEESEUNGJAE <karmaup0711@gmail.com>
2021-06-27 16:19:58 +0900

    add 역삼각형

--- star_pattern.js
+++ star_pattern.js
@@ -9,7 +9,7 @@ for (var i = 0; i < 10; i++) {
 console.log('<기본>');
 console.log(star);
 
-// 2개씩 추가되는 별찍기
+// 2개씩 추가
 var star = '';
 for (var i = 0; i < 10; i++) {
     for (var j = 0; j <= (i * 2); j++) {
@@ -19,3 +19,14 @@ for (var i = 0; i < 10; i++) {
 }
 console.log('<2개씩추가>');
 console.log(star);
+
+// 역삼각형 
+var star = '';
+for (var i = 10; i >= 0; i--) {
+    for (var j = 0; j <= i; j++) {
+        star += '*';
+    }
+    star += '\n';
+}
+console.log('<역삼각형>')
+console.log(star)
