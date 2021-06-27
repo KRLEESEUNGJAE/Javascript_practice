4dee6451768d5856ee44cbe7fe8abcf9f3631eef
KRLEESEUNGJAE <karmaup0711@gmail.com>
2021-06-27 22:11:34 +0900

    add Diamond

--- star_pattern.js
+++ star_pattern.js
@@ -28,13 +28,13 @@ for (var i = 10; i >= 0; i--) {
     }
     star += '\n';
 }
-console.log('<역삼각형>')
-console.log(star)
+console.log('<역삼각형>');
+console.log(star);
 
 // 가운데 정렬 삼각형
 var star = '';
-for (var i = 0; i < 10; i++) {
-    for (var j = 9; j > i; j--) {
+for (var i = 0; i < 5; i++) {
+    for (var j = 4; j > i; j--) {
         star += ' ';
     }
     for (var k = 0; k <= (i * 2); k++) {
@@ -42,5 +42,29 @@ for (var i = 0; i < 10; i++) {
     }
     star += '\n';
 }
-console.log('가운데정렬')
-console.log(star)
+console.log('가운데정렬');
+console.log(star);
+
+// 다이아몬드
+var star = '';
+for (var i = 0; i < 9; i++) {
+    if (i < 5) {
+        for (var j = 4; j > i; j--) {
+            star += ' ';
+        }
+        for (var k = 0; k <= i * 2; k++) {
+            star += '*';
+        }
+        star += '\n';
+    } else {
+        for (var j = 4; j < i; j++) {
+            star += ' ';
+        }
+        for (var k = 17; k > i * 2; k--) {
+            star += '*';
+        }
+        star += '\n';
+    }
+}
+console.log('다이아몬드')
+console.log(star);
