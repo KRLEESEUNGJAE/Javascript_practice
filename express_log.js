62bc84ae55685d7ed1d1b5fe7ca3b9bd2e6ad91f
KRLEESEUNGJAE <karmaup0711@gmail.com>
2021-08-09 00:53:58 +0900

    logger

--- express_log.js
+++ express_log.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
 const myLogger = function (req, res, next) {
-    //console.log('LOGGED'); // 이 부분을 req, res 객체를 이용해 고치면, 여러분들은 모든 요청에 대한 로그를 찍을 수 있습니다.
     console.log(`http request method is ${req.method}, url is ${req.url} `);
     next();
 };
